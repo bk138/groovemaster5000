6a6941008fdaa716bc3b47688584392990322e71
Christian Beier <info@christianbeier.net>
2020-10-22 21:25:54 +0200

    main.rs: add note sheet loading via C++

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use std::env;
 use std::process;
 
+// Notesheet shim
+extern "C" {
+    fn new_notesheet() -> *const libc::c_void;
+    fn notesheet_load_file(notesheet: *const libc::c_void, path: *const u8) -> libc::c_int;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("ERROR: no infile!");
         process::exit(1);
+    }
+
+    unsafe {
+        let notesheet = new_notesheet();
+        println!(
+            "noten ok? {}",
+            notesheet_load_file(notesheet, args[1].as_ptr())
+        );
     }
 }
