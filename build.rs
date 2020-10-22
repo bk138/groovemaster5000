5232695aec928b7a2a2bc43d522285ccd52972d3
Christian Beier <info@christianbeier.net>
2020-10-22 19:39:22 +0200

    build: compile and link relevant parts of the C++ codebase

---
+++ build.rs
@@ -0,0 +1,15 @@
+// build.rs
+
+fn main() {
+    cc::Build::new()
+        .cpp(true) // Switch to C++ library compilation.
+        .file("src/util.cpp")
+        .file("src/Sound.cpp")
+        .file("src/Notesheet.cpp")
+        .file("src/Mixer.cpp")
+        .file("src/MultModul.cpp")
+        .file("src/NoiseGen.cpp")
+        .file("src/PickedString.cpp")
+        .file("src/Instrument.cpp")
+        .compile("libgroovemaster.a");
+}
