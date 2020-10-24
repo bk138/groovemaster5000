ff582efbcc37fdc0077973c82f31ca095bfbc5b7
Christian Beier <info@christianbeier.net>
2020-10-24 12:11:35 +0200

    main.rs: implement driving a single picked string

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use std::env;
+use std::os;
 use std::process;
 
 mod note_sheet;
+
+// C++ PickedString shim
+extern "C" {
+    pub fn new_pickedstring(note: *const note_sheet::Note) -> *const os::raw::c_void;
+    pub fn pickedstring_is_done(pickedstring: *const os::raw::c_void) -> os::raw::c_int;
+    pub fn pickedstring_tick(pickedstring: *const os::raw::c_void);
+    pub fn pickedstring_get_output(pickedstring: *const os::raw::c_void) -> os::raw::c_double;
+}
+
+// C++ Sound shim
+extern "C" {
+    pub fn new_sound() -> *const os::raw::c_void;
+    pub fn sound_push_back(
+        sound: *const os::raw::c_void,
+        value: os::raw::c_double,
+    ) -> *const os::raw::c_void;
+    pub fn sound_save_file(sound: *const os::raw::c_void, path: *const u8) -> os::raw::c_int;
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -26,5 +45,16 @@ fn main() {
             "noten ok? {}",
             note_sheet::notesheet_load_file(notesheet, args[1].as_ptr())
         );
+
+        let output = new_sound();
+
+        let pickedstring = new_pickedstring(&note);
+
+        while pickedstring_is_done(pickedstring) == 0 {
+            pickedstring_tick(pickedstring);
+            sound_push_back(output, pickedstring_get_output(pickedstring));
+        }
+
+        sound_save_file(output, "out.aum\0".as_ptr()); //FIXME make use of CStr
     }
 }
