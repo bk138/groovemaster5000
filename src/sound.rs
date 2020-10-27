9f5123afbf9795ee834e33205fd972085ace2e71
Christian Beier <info@christianbeier.net>
2020-10-27 20:35:44 +0100

    sound.rs: remove C++ shim

--- src/sound.rs
+++ src/sound.rs
@@ -1,20 +1,6 @@
 use std::fs;
 use std::io::BufWriter;
 use std::io::Write;
-use std::os;
-
-// C++ Sound shim
-extern "C" {
-    pub fn new_sound() -> *const os::raw::c_void;
-    pub fn sound_push_back(
-        sound: *const os::raw::c_void,
-        value: os::raw::c_double,
-    ) -> *const os::raw::c_void;
-    pub fn sound_save_file(
-        sound: *const os::raw::c_void,
-        path: *const os::raw::c_char,
-    ) -> os::raw::c_int;
-}
 
 pub struct Sound {
     snddata: Vec<f64>,
