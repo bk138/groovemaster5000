749632b1ea1397953a33b13db9548ae394d599c3
Christian Beier <info@christianbeier.net>
2021-01-16 21:56:46 +0100

    noise_gen.rs: simplify

--- src/noise_gen.rs
+++ src/noise_gen.rs
@@ -2,26 +2,20 @@ use rand::Rng;
 
 pub struct NoiseGen {
     counter: i32,
-
-    pub output: f64,
 }
 
 impl NoiseGen {
     pub fn new(l: i32) -> NoiseGen {
-        NoiseGen {
-            output: 0.0,
-            counter: l,
-        }
+        NoiseGen { counter: l }
     }
 
     pub fn next(&mut self) -> f64 {
         if self.counter == 0 {
-            self.output = 0.0;
+            0.0
         } else {
             self.counter -= 1;
             // using this instead of stdlib.h's rand() makes the Rust version sound different
-            self.output = (-32768.0 + rand::thread_rng().gen_range(0.0, 65536.0)) / 32768.0;
+            (-32768.0 + rand::thread_rng().gen_range(0.0, 65536.0)) / 32768.0
         }
-        self.output
     }
 }
