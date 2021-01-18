e9060d0e8a4434d2cdce9866d3ff15167040ddc3
Christian Beier <info@christianbeier.net>
2021-01-18 21:34:21 +0100

    build: remove C++ building for Rust

--- build.rs
+++
@@ -1,9 +0,0 @@
-// build.rs
-
-fn main() {
-    cc::Build::new()
-        .cpp(true) // Switch to C++ library compilation.
-        .file("src/util.cpp")
-        .file("src/Instrument.cpp")
-        .compile("libgroovemaster.a");
-}
